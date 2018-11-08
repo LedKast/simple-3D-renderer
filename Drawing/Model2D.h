9e6f6ecfab85975b2c5503baf0e66dd4beaf9033
Alex <ledkast@gmail.com>
2018-11-08 16:18:21 +0300

    upd

--- Drawing/Model2D.h
+++ Drawing/Model2D.h
@@ -2,28 +2,93 @@
 #define MODEL2D_H
 
 #include <string>
+#include <fstream>
 #include "../Math/Matrix.h"
+#include "../Math/AffineTransform.h"
 
-/* Добавьте в класс Model2D следующие конструкторы и методы:
-    ◦ Model2D(string, string) – конструктор создания модели по именам
-        файлов, в которых лежат карта вершин и карта рёбер;
-    ◦ double GetVertexX(int) и double GetVertexY(int) – получение координат
-        вершины модели с заданным номером;
-    ◦ void Apply(Matrix<>) – применение к модели аффинного преобразова-
-        ния, заданного своей матрицей.
+/*
+ *      в матрице смежности размера M на M, состоящей из нулей и единиц,
+ *  – при этом на пересечении i-ой строки и j-ого столбца стоит единица,
+ *  если i-ая и j-ая вершины соединены ребром.
+ *
+ *      Кроме того, необходимо реализовать метод Apply(Matrix AT)
+ *  применения к модели аффинного преобразования, заданного матрицей AT.
+ *  В памяти должна храниться исходная матрица вершин модели и накопленное аффинное преобразование.
  * */
+
+// Вариант #6
 
 class Model2D
 {
-    Matrix<> Vertices; // вершины
-    Matrix<int> Edges; // ребра
+    Matrix<> vertices;      // вершины
+    Matrix<> cumulativeAT;  // накопленное аффинное преобразование
+    Matrix<> initVertices;  // исходные вершины
+
+    Matrix<int> edges;      // ребра
+
+    template <typename T = double>
+    Matrix<T> matrixFromFile(string file);
 
 public:
-    Model2D() : Vertices(), Edges() {}
-    Model2D(const Matrix<> Vertices, const Matrix<int> Edges) : Vertices(Vertices), Edges(Edges) {}
-    Matrix<> GetVertices() { return Vertices; }
-    Matrix<int> GetEdges() { return Edges; }
+    Model2D() : vertices(), edges(), initVertices(), cumulativeAT(identity()) {}
+    Model2D(const Matrix<> vert, const Matrix<int> edg) : vertices(vert), edges(edg), initVertices(), cumulativeAT(identity())
+    {initVertices = vert;}
+    Model2D(string vert, string edg);
+
+    // получение координат X,Y вершины под номером i
+    double getVertexX(int i) { return vertices(1, i); }
+    double getVertexY(int i) { return vertices(2, i); }
+    Matrix<> getVertices() { return vertices; }
+    Matrix<int> getEdges() { return edges; }
+
+    void apply(Matrix<> AT);
 };
+
+template<typename T>
+Matrix<T> Model2D::matrixFromFile(string file) {
+    fstream fs(file, ios::in); // входной поток
+    if (!fs.is_open()) {
+        cout << "CANT_OPEN_FILE::" << file;
+        return Matrix<T>(1, 1);
+    }
+
+    string temp;
+    int rows = 1, cols = 1;
+    getline(fs, temp);
+    for(char i : temp)
+        if (i == ' ')
+            cols++;
+    while(getline(fs, temp))
+        rows++;
+    fs.close();
+
+    fs.open(file, ios::in);
+    T *list = new T[rows*cols];
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+            fs >> list[i*cols + j];
+
+    Matrix<T> mtemp(rows, cols, list);
+    delete[] list;
+    fs.close();
+
+    return mtemp;
+}
+
+Model2D::Model2D(string vert, string edg) // конструктор задания данных из файла
+{
+    cumulativeAT = identity();
+    initVertices = vertices = matrixFromFile<>(vert);
+    edges = matrixFromFile<int>(edg);
+
+    cout << "VERTICES: \n" << vertices; // DEBUG
+    cout << "EDGES: \n" << edges; // DEBUG
+}
+
+void Model2D::apply(Matrix<> AT) {
+    cumulativeAT = AT * cumulativeAT;
+    vertices = cumulativeAT * initVertices;
+}
 
 
 #endif // MODEL2D_H
--- main.cpp
+++ main.cpp
@@ -1,159 +1,50 @@
-﻿#include <windows.h>
-#include <windowsx.h>
-
-
-#include "Data.h"
-
-#include "Drawing/Scene2D.h"
-#include "Drawing/Model2D.h"
+﻿#include <iostream>
 
 #include "Math/Matrix.h"
 #include "Math/AffineTransform.h"
+#include "Drawing/Model2D.h"
 
+#define PROJECTPATH "d:/DOCs/3_course/CGraphics/Lab_2_2D_Scene/"
 
-#define WINW 480
-#define WINH 320
+using namespace std;
 
-LRESULT _stdcall WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);						// прототип оконной процедуры
-int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)		// основнаRя процедура
+int main()
 {
-	// ѕервая составляющая часть основной процедуры - создание окна: сначала описывается оконный класс wc, затем создаЄтся окно hWnd
-	WNDCLASS wc;
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;						// имя оконной процедуры, закрепленной за данным классом
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;						// идентификатор приложения, содержащий адрес начала выделенной ему области памяти
-	wc.hIcon = LoadIcon(nullptr, IDI_APPLICATION);
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wc.hbrBackground = (HBRUSH)(6);
-	wc.lpszMenuName = 0;							// меню в оконном классе отсутствует
-	wc.lpszClassName = (LPCSTR)"MainWindowClass";	// имя оконного класса, используемое при создании экземпляров окна
-	RegisterClass(&wc);								// регистрация класса wc
+//	double a[6] = {
+//	    1, -2, 7,
+//        3, 0, 6};
+//	Matrix<> A(2, 3, a);
+//
+//	double b[6] = {
+//        2, 2,
+//        0, 1,
+//        7, 7};
+//	Matrix<> B(3, 2, b);
+//
+//	Matrix<> C = A*B;
+//	cout
+//		<< endl <<   "A =" << endl << A
+//		<< endl <<   "B =" << endl << B
+//		<< endl <<   "C =" << endl << C
+//		<< endl << "A+B =" << endl << (A+B)
+//		<< endl << "A-B =" << endl << (A-B)
+//		<< endl << "T(1,2) =" << endl << translation(1, 2);
 
-	HWND hWnd = CreateWindow(						// hWnd - дескриптор, идентифицирующий окно; функция создания окна заполняет дескриптор hWnd ненулевым значением
-		(LPCSTR)"MainWindowClass",					// имя оконного класса
-		(LPCSTR)"Plot2D Viewer",					// заголовок окна
-		WS_OVERLAPPEDWINDOW,						// стиль окна
-		200,200, WINW + 200, WINH + 200,			// координаты на экране левого верхнего угла окна, его ширина и высота
-		nullptr,nullptr,hInstance,nullptr);
+    //Model2D model1(PROJECTPATH "Model1_vert.txt",PROJECTPATH "Model1_edg.txt");
 
-	ShowWindow(hWnd,nCmdShow);
-	UpdateWindow(hWnd);
+    double v[6] = {2, 1, 3, 0, 1, 1};
+    Matrix<> V(3, 2, v);
+    int e[2] = {1, 2};
+    Matrix<int> E(1, 2, e);
+    Model2D model(V, E);
+    cout
+            << endl << "Edges:" << endl << model.getEdges()
+            << endl << "Vertices before AT:" << endl << model.getVertices();
+    model.apply(translation(1, 2));
 
-	// ¬торая составляющая часть основной процедуры - основной цикл обработки системных сообщений, который ожидает сообщения и рассылает их соответствующим окнам
-	MSG msg;
-	while(GetMessage(&msg,nullptr,0,0))				// функция GetMessage выбирает из очереди сообщение и заносит его в структуру msg
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);						// функция DispatchMessage оповещает систему о необходимости вызова оконной процедуры
-	}
+    cout
+            << endl << "Vertices after AT:" << endl << model.getVertices();
 
-	return 0;
-}
 
-// ¬ основном модуле объявляется только одна глобальная переменная - создаЄтся объект класса Scene2D
-// ¬се дальнейшие действия осуществляются посредством обращения к методам, реализованным в этом классе
-Scene2D scene(WINW/2, WINH/2, DEFSCALE, DEFSCALE + 50);
-int plotType = 1;
-
-LRESULT _stdcall WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)		// оконная процедура принимает и обрабатывает все сообщения, отправленные окну
-{
-	switch(msg)
-	{
-		case WM_PAINT:
-		{
-			HDC dc = GetDC(hWnd);
-			scene.Clear(dc);				// вызов реализованного в классе Camera2D метода, отвечающего за очистку рабочей области окна hWnd
-			if (plotType)
-			{
-				scene.plot(dc, sinusoid, RED);
-				scene.plot(dc, parabola, BLUE);
-				scene.plot(dc, test, ORANGE);
-			}
-			else
-			{
-				scene.polarPlot(dc, polarCos, BLUE);
-				scene.polarPlot(dc, polarRose, ORANGE);
-				scene.polarPlot(dc, polarSpiral, RED);
-			}
-			ReleaseDC(hWnd,dc);
-			return DefWindowProc(hWnd,msg,wParam,lParam);
-		}
-
-		case WM_SIZE:
-		{
-			HDC dc = GetDC(hWnd);
-			scene.SetResolution(dc);
-			ReleaseDC(hWnd,dc);
-			InvalidateRect(hWnd,nullptr,false);
-			return 0;
-		}
-		case WM_KEYDOWN:
-		{
-			switch (wParam)
-			{
-				case 0x31: //1 key
-				{
-					plotType = 1;
-					break;
-				}
-				case 0x32: //2 key
-				{
-					plotType = 0;
-					break;
-				}
-			}
-			InvalidateRect(hWnd, nullptr, false);
-			return 0;
-		}
-		case WM_LBUTTONDOWN:
-		{
-			scene.StartDragging(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-			return 0;
-		}
-		case WM_LBUTTONUP:
-		{
-			scene.StopDragging();
-			return 0;
-		}
-		case WM_MOUSEMOVE:
-		{
-			if (scene.isDragging)
-			{
-				scene.drag(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
-				InvalidateRect(hWnd, nullptr, false);
-			}
-			break;
-		}
-
-		case WM_MOUSEWHEEL:
-		{
-			POINT pt;
-			GetCursorPos(&pt);
-			ScreenToClient(hWnd, &pt);
-
-			int x = GET_X_LPARAM(lParam);
-			int y = GET_Y_LPARAM(lParam);
-			if (GET_WHEEL_DELTA_WPARAM(wParam) > 0) //UP
-				scene.scale(pt.x, pt.y, true);
-			else //DOWN
-				scene.scale(pt.x, pt.y, false);
-
-			InvalidateRect(hWnd, nullptr, false);
-			return 0;
-		}
-
-		case WM_DESTROY:
-		{
-			PostQuitMessage(0);
-			return 0;
-		}
-
-		default:
-		{
-			return DefWindowProc(hWnd,msg,wParam,lParam);
-		}
-	}
 	return 0;
 }
